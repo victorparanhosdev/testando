8f8b76fe6db8fba6d56088595a0848921de6acf2
Victor Paranhos <victor_paranhos@hotmail.com>
2023-05-24 15:50:53 -0300

    .

---
+++ main.js
@@ -0,0 +1,14 @@
+
+import { Router } from "./route.js";
+
+const router = new Router();
+
+router.add("/", "/pages/inicio.html");
+router.add("/cadastro", "/pages/cadastro.html");
+router.add("/consultas", "/pages/consultas.html");
+router.add("/contatos", "/pages/contatos.html");
+
+window.route = () => router.route()
+window.onpopstate = () => router.route();
+
+
